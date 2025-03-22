2fe21b3a2ada8bb9ea18870003efea373608d5fa
Ahn JeongSu <60861839+hoeunsu3@users.noreply.github.com>
2025-03-22 13:20:14 +0900

    Update getFourierPath.m

--- getFourierPath.m
+++ getFourierPath.m
@@ -1,22 +1,22 @@
-function reconstructed_path = getFourierPath(M, coeffs)
+function reconstructed_path = getFourierPath(numPath, coeffs)
     % Reconstruct the path using Fourier series (vectorized).
     %
     % Inputs:
-    %   coeffs - (N+1)x1 complex Fourier coefficients.
-    %   M      - (optional) number of points to reconstruct (default: 1000)
+    %   coeffs  - (N+1)x1 complex Fourier coefficients.
+    %   numPath - (optional) number of points to reconstruct (default: 1000)
     %
     % Output:
     %   reconstructed_path - (Mx1) complex vector (path in complex plane)
 
     if nargin < 2
-        M = 1000; % default number of points
+        numPath = 1000; % default number of points
     end
 
     N = length(coeffs) - 1;
     n = 0:N;
     k = (-1).^n .* ceil(n / 2);  % Frequency indices (1x(N+1))
 
-    t = (0:M-1)' / M;            % Normalized time values (Mx1)
+    t = (0:numPath-1)' / numPath;            % Normalized time values (Mx1)
     angle_matrix = 2 * pi * t * k;  % Mx(N+1)
 
     reconstructed_path = exp(1i * angle_matrix) * coeffs;  % (Mx1)
